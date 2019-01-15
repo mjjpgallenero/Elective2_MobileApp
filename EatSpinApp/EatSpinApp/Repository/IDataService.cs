2a6ac8cf5affb1a8537e5ce40cd00cd2470149ae
mjjpgallenero <mjjpgallenero@addu.edu.ph>
2019-01-15 12:37:13 +0800

    Tried implementing database in LocalDataService

--- EatSpinApp/EatSpinApp/Repository/IDataService.cs
+++ EatSpinApp/EatSpinApp/Repository/IDataService.cs
@@ -3,10 +3,9 @@
     public interface IDataService<T> where T: class
     {
         void Add(T record);
-
-        //T Get();
-        //int Update(T record);
-        //int Delete(T record);
+        T Get();
+        int Update(T record);
+        int Delete(T record);
 
     }
 }
--- EatSpinApp/EatSpinApp/Repository/LocalRepository/LocalDataService.cs
+++ EatSpinApp/EatSpinApp/Repository/LocalRepository/LocalDataService.cs
@@ -1,10 +1,33 @@
-﻿namespace EatSpinApp.Repository.LocalRepository
+﻿using System;
+using System.IO;
+using EatSpinApp.Models;
+using SQLite;
+
+namespace EatSpinApp.Repository.LocalRepository
 {
     public class LocalDataService<T> : IDataService<T> where T : class, new()
     {
+        //private static readonly string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal));
+        //private SQLiteConnection db = new SQLiteConnection(dbPath);
+
         public void Add(T record)
         {
-            throw new System.NotImplementedException();
+            throw new NotImplementedException();
+            //db.CreateTable<Restaurant>();
+        }
+        public T Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(T record)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete(T record)
+        {
+            throw new NotImplementedException();
         }
     }
 }
