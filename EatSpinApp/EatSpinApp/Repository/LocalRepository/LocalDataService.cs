b4522eea09bfd941a502ed4e0acf03d134e8cb83
mjjpgallenero <mjjpgallenero@addu.edu.ph>
2019-01-15 11:57:57 +0800

    Added classes in LocalRepository folder
    Added LocalDataService and LocalRepository classes, following format from email reference

---
+++ EatSpinApp/EatSpinApp/Repository/LocalRepository/LocalDataService.cs
@@ -0,0 +1,10 @@
+﻿namespace EatSpinApp.Repository.LocalRepository
+{
+    public class LocalDataService<T> : IDataService<T> where T : class, new()
+    {
+        public void Add(T record)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ EatSpinApp/EatSpinApp/Repository/LocalRepository/LocalRepository.cs
@@ -0,0 +1,9 @@
+﻿using EatSpinApp.Models;
+
+namespace EatSpinApp.Repository.LocalRepository
+{
+    public class LocalRepository : IRepository
+    {
+        public IDataService<Restaurant> Restaurant { get; } = new LocalDataService<Restaurant>();
+    }
+}
