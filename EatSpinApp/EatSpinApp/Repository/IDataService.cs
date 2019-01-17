810933b69678466b39f396718e0ae0c90a4c78f5
mjjpgallenero <mjjpgallenero@addu.edu.ph>
2019-01-17 12:23:02 +0800

    Implemented database get and getrange, added gitignore

--- EatSpinApp/EatSpinApp/Repository/IDataService.cs
+++ EatSpinApp/EatSpinApp/Repository/IDataService.cs
@@ -1,9 +1,15 @@
-﻿namespace EatSpinApp.Repository
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace EatSpinApp.Repository
 {
     public interface IDataService<T> where T: class
     {
         void Add(T record);
-        T Get();
+        T Get(Expression<Func<T, bool>> condition = null);
+        List<T> GetRange(Expression<Func<T, bool>> condition = null);
         int Update(T record);
         int Delete(T record);
 
--- EatSpinApp/EatSpinApp/Repository/LocalRepository/LocalDataService.cs
+++ EatSpinApp/EatSpinApp/Repository/LocalRepository/LocalDataService.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq.Expressions;
 using EatSpinApp.Models;
 using SQLite;
 
@@ -7,17 +9,32 @@ namespace EatSpinApp.Repository.LocalRepository
 {
     public class LocalDataService<T> : IDataService<T> where T : class, new()
     {
-        //private static readonly string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal));
+        private static readonly string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal));
         //private SQLiteConnection db = new SQLiteConnection(dbPath);
 
         public void Add(T record)
         {
-            throw new NotImplementedException();
-            //db.CreateTable<Restaurant>();
+            using (var db = new SQLiteConnection(dbPath))
+            {
+                db.CreateTable<Restaurant>();
+                db.Insert(record);
+            }
         }
-        public T Get()
+
+        public T Get(Expression<Func<T, bool>> condition)
         {
-            throw new NotImplementedException();
+            using (var db = new SQLiteConnection(dbPath))
+            {
+                return db.Table<T>().FirstOrDefault(condition);
+            }
+        }
+
+        public List<T> GetRange(Expression<Func<T, bool>> condition = null)
+        {
+            using (var db = new SQLiteConnection(dbPath))
+            {
+                return db.Table<T>().Where(condition).ToList();
+            }
         }
 
         public int Update(T record)
